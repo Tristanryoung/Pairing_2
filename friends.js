4b84646fb99e6f2ec5abc9ec4e38e27210643e10
Tristan Young <tyoung22@turing.edu>
2023-01-11 17:12:47 -0800

    Add array method pop

--- friends.js
+++ friends.js
@@ -1,4 +1,7 @@
-var arrays = ["1","2","3", "4", "5"]
-console.log(arrays[2])
-console.log(arrays.slice(2, -2))
-console.log(arrays[0])
+// var arrays = ["1","2","3", "4", "5"]
+// console.log(arrays[2])
+// console.log(arrays.slice(2, -2))
+// console.log(arrays[0])
+var places = ["Tokyo", "Rio", "Moscow", "Los Angeles", "Toronto"]
+places.pop()
+console.log(places)
